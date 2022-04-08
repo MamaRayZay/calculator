18845ecd7998dec14cf68e23e22c4fb3b046ae2f
MamaRayZay <kemiakinrotimi1988@yahoo.com>
2022-04-07 20:18:42 -0600

    I added a background color

--- calc.js
+++ calc.js
@@ -68,46 +68,11 @@
 //   } 
 // }
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[signs]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+// const numberButtons = document.querySelectorAll('[data-number]')
+// const operationButtons = document.querySelectorAll('[signs]')
+// const equalsButton = document.querySelector('[data-equals]')
+// const deleteButton = document.querySelector('[data-delete]')
+// const allClearButton = document.querySelector('[data-all-clear]')
+// const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+// const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
-class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
-    this.clear()
-      this.currentOperand = ''
-      this.previousOperand = ''
-      this.operation = undefined
-    
-};
-clear() {
-}
-
-delete() {
-}
-
-appendNumber(number) {
-}
-
-chooseOperation(operation) {
-}
-
-compute() {
-}
-
-updateDisplay() {
-}
-// const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
-
-// numberButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     calculator.appendNumber(button.innerText)
-//     calculator.updateDisplay()
-//   })
-// })
